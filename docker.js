414e1907b5bbc31774611c273a9c2f6882f6807a
Skncoe1 <blablablacksheep231@gmail.com>
2023-05-31 00:06:10 +0530

    first

---
+++ docker.js
@@ -0,0 +1,22 @@
+console.log("Hello World"); 
+
+/*
+
+1.Open Docker desktop
+2.click on dev-environments
+3.Create a dev-environment - give local directory path
+4.open in vscode(if open in vscode not detectded , open vscode and install docker extension)
+5.open terminal 
+6.mkdir new-app
+7.cd new-app
+8.add dockerfile and docker.js in new-app folder
+9.on terminal>new-app> docker build -t new-app .
+10.terminal>new-app> docker images
+11.terminal>new-app>docker run newapp
+12.open docker desktop
+13.open images
+14.open image(name must the one which you have mention while creating dev-env
+15.on right side , click in play button
+16.voila Hello world
+
+*/
